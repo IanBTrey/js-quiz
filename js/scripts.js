3a6b91d10faba04c311a2010c9cadffe2a371180
IanBTrey <kibetkirui010@gmail.com>
2019-02-25 09:59:00 +0300

    making some changes on my busines logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-//==== Business logic====//
 $(document).ready(function() {
 
   $("form#mumbiForm").submit(function(event){
+     event.preventDefault();
     var qNI = $("input:radio[name=jquery]:checked").val();
     var qNII = $("input:radio[name=js]:checked").val();
     var qNIII = $("input:radio[name=founder]:checked").val();
@@ -13,18 +13,25 @@ $(document).ready(function() {
     var qNIX = $("input:radio[name=create]:checked").val();
     var qNX = $("input:radio[name=programming]:checked").val();
 
-    var totalScore = parseInt(qNI)+parseInt(qNII)+parseInt(qNIII)+parseInt(qNIV)+parseInt(qNV)+parseInt(qNVI)+parseInt(qNVII)+parseInt(
-      qNVIII)+parseInt(qNIX)+parseInt(qNX);
-   $("#totalScore").text("Bonjour! THIS IS WHAT YOU GET AFTER THE QUIZ!  ===>> " + totalScore + " points out of a possible 100 points");
 
-  
+    var totalScore =parseInt(qNI)+parseInt(qNII)+parseInt(qNIII)+parseInt(qNIV)+parseInt(qNV)+parseInt(qNVI)+parseInt(qNVII)+
+      parseInt(qNVIII)+parseInt(qNIX)+parseInt(qNX);
+
+   if("#totalScore").text("Bonjour! THIS IS WHAT YOU GET AFTER THE QUIZ!  ===>> " + totalScore + " points out of a possible 100 points"){
+     $("#totalScore").text("select all options");
+   }
+
+    
 
 
     //==== User Interface ====//
 
-    $("form#mumbiForm").slideUp(1550);
-    $("#totalScore").show(4050);
+    $("form#mumbiForm").slideUp(300);
+    $("#totalScore").show(5000);
+
      event.preventDefault();
+
+
   });
 
 });
